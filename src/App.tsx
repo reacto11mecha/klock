fca31f30480496e3e7ce12e94abfd5036f5b23cc
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-20 20:16:06 +0700

    fix: change wrong value and type

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { onMount, createSignal, createMemo } from "solid-js";
 import type { Component } from "solid-js";
 
 const updateString = (time: number): string =>
-  time < 9 ? `0${time as unknown as string}` : (time as unknown as string);
+  time < 10 ? `0${time as unknown as string}` : (time as unknown as string);
 
 const App: Component = () => {
   const [time, setTime] = createSignal<Date>(new Date());
 
-  const hour = createMemo<String>(() => updateString(time().getHours()));
-  const minute = createMemo<String>(() => updateString(time().getMinutes()));
-  const second = createMemo<String>(() => updateString(time().getSeconds()));
+  const hour = createMemo<string>(() => updateString(time().getHours()));
+  const minute = createMemo<string>(() => updateString(time().getMinutes()));
+  const second = createMemo<string>(() => updateString(time().getSeconds()));
 
   onMount(() => {
     function updateTime() {
@@ -22,7 +22,9 @@ const App: Component = () => {
 
   return (
     <div>
-      {hour()}:{minute()}:{second()}
+      <p>
+        {hour()}:{minute()}:{second()}
+      </p>
     </div>
   );
 };
