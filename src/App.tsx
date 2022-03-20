4505a9d27ec61f450b3a958dca52f675a290f711
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-20 21:58:39 +0700

    feat: creating basic page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { onMount, createSignal, createMemo } from "solid-js";
 import type { Component } from "solid-js";
 
+import Card from "./components/Card";
+
+import styles from "./styles/App.module.css";
+
 const updateString = (time: number): string =>
-  time < 10 ? `0${time as unknown as string}` : (time as unknown as string);
+  time < 10 ? `0${(time as unknown) as string}` : ((time as unknown) as string);
 
 const App: Component = () => {
   const [time, setTime] = createSignal<Date>(new Date());
@@ -21,10 +25,8 @@ const App: Component = () => {
   });
 
   return (
-    <div>
-      <p>
-        {hour()}:{minute()}:{second()}
-      </p>
+    <div class={styles.container}>
+      <Card hour={hour} minute={minute} second={second} />
     </div>
   );
 };
