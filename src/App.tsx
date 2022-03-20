038bf433a5f4360e93cc23abfbc9cb5e6a77fa39
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2022-03-20 20:05:52 +0700

    feat: add core functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import type { Component } from 'solid-js';
+import { onMount, createSignal, createMemo } from "solid-js";
+import type { Component } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+const updateString = (time: number): string =>
+  time < 9 ? `0${time as unknown as string}` : (time as unknown as string);
 
 const App: Component = () => {
+  const [time, setTime] = createSignal<Date>(new Date());
+
+  const hour = createMemo<String>(() => updateString(time().getHours()));
+  const minute = createMemo<String>(() => updateString(time().getMinutes()));
+  const second = createMemo<String>(() => updateString(time().getSeconds()));
+
+  onMount(() => {
+    function updateTime() {
+      setTime(new Date());
+      window.requestAnimationFrame(updateTime);
+    }
+
+    updateTime();
+  });
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div>
+      {hour()}:{minute()}:{second()}
     </div>
   );
 };
